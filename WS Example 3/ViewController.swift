a69c22d0792619904948fd932c42e06bd20b2ec3
louiskonig <louiskonig@gmail.com>
2014-12-06 19:12:55 -0500

    Initial Commit

---
+++ WS Example 3/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WS Example 3
+//
+//  Created by Louis Konig on 12/6/14.
+//  Copyright (c) 2014 Louis Konig. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
